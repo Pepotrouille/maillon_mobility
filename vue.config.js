92596110fe1c7f0598944de70d3d30d6e12399cb
Pepotrouille <mechaneka83@gmail.com>
2024-11-27 16:42:29 +0100

    fix: hopefully blank page

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,19 @@
 // vue.config.js file to be placed in the root of your repository
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import Main from '@/components/Main.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {path: publicPath, name: 'Main', component: Main},
+    //...
+  ],
+  mode: 'hash',
+})
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/maillon_mobility/'
-      : '/'
+    ? '/maillon_mobility/'
+    : '/'
   }
-  
